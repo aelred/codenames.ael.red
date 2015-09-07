9e94cdeb526435c93faa7d120b9c210529645ad9
Felix Chapman <aelred717@gmail.com>
2015-09-07 19:40:10 +0100

    Add colours to game grid

--- codenames.js
+++ codenames.js
@@ -6,10 +6,17 @@ $(document).ready(function() {
 
     $.get('words.txt', function(data) {
         words = data.split("\n");
+        colors = [
+            'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
+            'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
+            'neutral', 'neutral', 'neutral', 'neutral', 'neutral', 'neutral',
+            'neutral', 'assassin'
+        ];
 
         // Iterate over and populate table cells
         $("#grid tr").each(function() {
             $('td', this).each(function() {
+                $(this).addClass(randomPop(colors));
                 $(this).append(randomPop(words));
             });
         });
